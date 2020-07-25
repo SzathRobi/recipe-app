9298a3fad495c559657389c0f90a2ea632fc7de3
DevVatu <devvatu@gmail.com>
2020-07-25 11:15:19 +0200

    Updates

---
+++ precache-manifest.f1b636de307ac85e4e00c4ac962d440d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b29319acb410917d8cf4f0d6c6cd7443",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "21cff9ce8b98c9b92839",
+    "url": "/recipe-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "70bc0443bb549e04fed1",
+    "url": "/recipe-app/static/js/2.ffb7c972.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.ffb7c972.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21cff9ce8b98c9b92839",
+    "url": "/recipe-app/static/js/main.5767dc61.chunk.js"
+  },
+  {
+    "revision": "cd77bb1956273d314d45",
+    "url": "/recipe-app/static/js/runtime-main.14680ac4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/recipe-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
